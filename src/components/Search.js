07309d7b8bb45e689caac7d8cce0546b8965a870
LaureanoVera <laureanoigvera@gmail.com>
2021-07-27 08:39:06 -0300

    proyecto listo para deploy

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./Search.module.css";
 import { ImSearch } from "react-icons/im";
 import { useHistory } from "react-router";
